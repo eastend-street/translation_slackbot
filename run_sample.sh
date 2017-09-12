4cb525860080490775361ef84fba132d800ea6c4
yfp5521 <junya_yamada@bizocean.co.jp>
2017-09-12 17:59:14 +0900

    third commit

--- run_sample.sh
+++ run_sample.sh
@@ -1,12 +1,13 @@
-# any_value : choice your any_value that written in build_sample.sh
+# any_value : your any_value that written in build_sample.sh
 sudo docker stop `sudo docker ps -f ancestor=any_value:latest -q`$
 
 sudo docker run -it \
-        #  any_key_value : choice your api key file
+        # any_key_value : your api key file
         -e "GOOGLE_APPLICATION_CREDENTIALS=keys/any_key_value" \
-        -v /home/BIZOCEAN/junya_yamada/botForSlack20170906/app:/app \
-        # 0000:0000 : choice your port number of Docker
+        # any_directory : own directory
+        -v /home/any_directory/app:/app \
+        # 0000:0000 : your port number of Docker
         -p 0000:0000 \
-        # any_value : choice your any_value that written in build_sample.sh
+        # any_value : your any_value that written in build_sample.sh
         any_value \
         bash
